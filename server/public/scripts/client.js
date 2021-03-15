51e16f41b9e5158097bf3604a78a0729a4ccb5f0
Kris Szafranski <kris@thoughtshelter.com>
2021-03-15 10:19:33 -0500

    working inputs, ready for ajax

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,5 +2,26 @@
 $(document).ready( start );
 
 function start() {
-    console.log('ready to start!');
+    console.log('DOM is ready');
+    // event listeners, event handlers (the function called)
+    $('#submitCalculation').on('click', submitCalculation);
+}
+
+
+function submitCalculation( ){
+    console.log('clicked submit');
+
+    // get input values
+    // get operation
+    // package these up
+    const newCalculation = {
+        xInput: $('#xInput').val(),
+        yInput: $('#yInput').val(),
+        operator: $('#operator').val(),
+    };
+
+    console.log('new calc: ', newCalculation);
+
+    // send to the server
+    
 }
